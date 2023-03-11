36c3a0536e653355626320649bfa5775a0836dcd
Kyle Jones <kjones901@gmail.com>
2023-03-11 16:32:32 -0700

    Worked with Josh Davis and Carly Staheli to continue working through and complete the Javascript coding. We adjusted the prompts to make an array with selected arrays instead of spreading them out. This allowed us to create for loops to select digits from within the user selected arrays to be added to the password. The first for loop selects one item from each selected array to make sure it is validated or present in the final password. Then the second for loop fills in random characters from the selected arrays to the user inputed length.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,10 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}","~"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var combined = [];
 
-
-
-// Write password to the #password input
 function writePassword() {
 
   var password = generatePassword();
@@ -17,8 +13,23 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function passwordLength(numAmount) {
+  var str = '';
+  for(var i = 0; i <combined.length; i++){
+    var arr = combined[i];
+    var index = Math.floor(Math.random() * (arr.length-1));
+    str += arr[index];
+  }
 
+  for(var i = 0; i < numAmount - combined.length; i++) {
+    var index = Math.floor(Math.random() * (combined.length-1));
+    var arr = combined[index];
+    var value = Math.floor(Math.random() * (arr.length-1));
+    str += arr[value];
+  }
 
+  return str;
+}
 
 function generatePassword(){
   var lengthP = prompt("How long would you like your password? (8 - 128 Characters)")
@@ -26,42 +37,31 @@ function generatePassword(){
   var lowercaseP = confirm("Would you like lowercase characters?")
 
       if (lowercaseP == true) {
-        combined.push(...lowercase);
+        combined.push(lowercase);
       } 
   
   var uppercaseP = confirm("Would you like UPPERCASE characters?")
 
       if (uppercaseP == true) {
-        combined.push(...uppercase);
+        combined.push(uppercase);
       }
 
   var specCharsP = confirm("Would you like special characters?")
 
       if (specCharsP == true) {
-        combined.push(...specChars);
+        combined.push(specChars);
       }
 
   var numbersP = confirm("Would you like numeric characters?")
 
       if (numbersP == true) {
-        combined.push(...numbers);
-        // console.log(combined);
+        combined.push(numbers);
       } 
 
-  return combined
+      var answer = passwordLength(lengthP);
+      console.log(answer);
+
+  return answer
   }
  
-  function passwordLength(numAmount) {
-    var str = '';
-    for(var i = 0; i < numAmount; i++)
-    str += combined[i]
-    console.log(combined);
-  }
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// console.log(Math.random()) // number from 0 - 1
-
-// console.log(Math.random() * 6) //number between 0 - 6
