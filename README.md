5e3bbec636c4f7b9a93e0a0a25365b01d6ee43b2
Kyle Jones <kjones901@gmail.com>
2023-03-18 15:30:39 -0600

    fixed a some bugs including clearing array without needing to refresh the page, and limiting user inputs to 8-128 characters and requiring a number input

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Password Generator
 
-# Password Generator
-
 ## The purpose of this assignment was to write javascript code that allows a user to generate a password with specific character types that the user specifies with window prompts. 
 
 This challange expanded my javascript understanding and helped solidify what I have been learning in class.
 
 Screen Shot:
-![Alt text](/Assets/screenshot.png?raw=true "Screen Shot")
+![Alt text](/assets/screenshot.png?raw=true "Screen Shot")
 
 https://kjones901.github.io/password-gen/
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Variables containing arrays of character Types
+
 var generateBtn = document.querySelector("#generate");
 var specChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}","~"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,29 +15,42 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
+
+// Functions that pull one character from each array type selected by user. And then continues to randomly fill to the length designated by user.
 function passwordLength(numAmount) {
   var str = '';
-  for(var i = 0; i <combined.length; i++){
+  for(var i = 0; i < combined.length; i++){
     var arr = combined[i];
-    var index = Math.floor(Math.random() * (arr.length-1));
+    var index = Math.floor(Math.random() * (arr.length));
     str += arr[index];
   }
 
   for(var i = 0; i < numAmount - combined.length; i++) {
-    var index = Math.floor(Math.random() * (combined.length-1));
+    var index = Math.floor(Math.random() * (combined.length));
     var arr = combined[index];
-    var value = Math.floor(Math.random() * (arr.length-1));
+    var value = Math.floor(Math.random() * (arr.length));
     str += arr[value];
   }
 
   return str;
 }
 
+
+
 function generatePassword(){
+// Clears the array if an array is already present and redirects the user to input a number value if they input something else and to set a length between 8 and 128. 
+
+  combined = [];
+  console.log(combined);
   var lengthP = prompt("How long would you like your password? (8 - 128 Characters)")
+    if(lengthP < 8 || lengthP > 128 || isNaN(lengthP) ) {
+      alert("Password must be between 8-128 Characters")
+      return "Try Again"
+    }
 
+  //Goes through each character type pushing the arrays of selected character types into a larger array to draw characters from.
   var lowercaseP = confirm("Would you like lowercase characters?")
-
       if (lowercaseP == true) {
         combined.push(lowercase);
       } 
